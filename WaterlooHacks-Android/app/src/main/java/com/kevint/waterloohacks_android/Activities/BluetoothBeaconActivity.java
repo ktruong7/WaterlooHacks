c450e6c3245d5c54193c0604c56f55d17b14bd2b
Kevin Truong <truong.kevin3@gmail.com>
2016-01-23 14:33:44 -0500

    Added a broadcast receiver to update the ui with messages when a bluetooth beacon is detected.

--- WaterlooHacks-Android/app/src/main/java/com/kevint/waterloohacks_android/Activities/BluetoothBeaconActivity.java
+++ WaterlooHacks-Android/app/src/main/java/com/kevint/waterloohacks_android/Activities/BluetoothBeaconActivity.java
@@ -1,5 +1,9 @@
 package com.kevint.waterloohacks_android.Activities;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,15 +13,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.kevint.waterloohacks_android.Adapters.BeaconMessageListAdapter;
 import com.kevint.waterloohacks_android.R;
 
+import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
+
+import java.util.Collection;
 
 public class BluetoothBeaconActivity extends AppCompatActivity implements BeaconConsumer{
     protected static final String TAG = "BluetoothBeaconActivity";
@@ -29,6 +37,16 @@ public class BluetoothBeaconActivity extends AppCompatActivity implements Beacon
     private Button testMessageButton;
 
     private BeaconManager beaconManager;
+
+    // This broadcast receiver is used to update the activity ui when the phone receives data
+    // from a bluetooth beacon.
+    BroadcastReceiver messageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            messageListAdapter.addMessage(intent.getStringExtra("message"));
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,7 +58,7 @@ public class BluetoothBeaconActivity extends AppCompatActivity implements Beacon
             @Override
             public void onClick(View v) {
                 index += 1;
-                messageListAdapter.addMessage("test message "+index);
+                messageListAdapter.addMessage("test message " + index);
             }
         });
         messageList.setAdapter(messageListAdapter);
@@ -48,9 +66,17 @@ public class BluetoothBeaconActivity extends AppCompatActivity implements Beacon
         beaconManager = BeaconManager.getInstanceForApplication(this);
         // To detect proprietary beacons, you must add a line like below corresponding to your beacon
         // type.  Do a web search for "setBeaconLayout" to get the proper expression.
-        // beaconManager.getBeaconParsers().add(new BeaconParser().
-        //        setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+//         beaconManager.getBeaconParsers().add(new BeaconParser().
+//                setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("m:0-3=4c000215,i:4-19,i:20-21,i:22-23,p:24-24"));
         beaconManager.bind(this);
+
+        // register broadcast receiver
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("com.intent.MESSAGE_INTENT");
+        registerReceiver(messageReceiver , filter);
+
     }
 
     @Override
@@ -79,31 +105,57 @@ public class BluetoothBeaconActivity extends AppCompatActivity implements Beacon
     protected void onDestroy() {
         super.onDestroy();
         beaconManager.unbind(this);
+        unregisterReceiver(messageReceiver);
     }
 
     @Override
     public void onBeaconServiceConnect() {
+        beaconManager.setRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                if (beacons.size() > 0) {
+                    Log.i(TAG, "The first beacon I see is about "+beacons.iterator().next().getDistance()+" meters away.");
+                    broadcastIntent("The first beacon I see is about " + beacons.iterator().next().getDistance()+" meters away.");
+                }
+            }
+        });
+
         beaconManager.setMonitorNotifier(new MonitorNotifier() {
             @Override
             public void didEnterRegion(Region region) {
                 Log.i(TAG, "I just saw an beacon for the first time!");
-                messageListAdapter.addMessage("Detected a beacon: " + region.getUniqueId());
+                broadcastIntent("Detected a beacon with id: " + region.getUniqueId());
             }
 
             @Override
             public void didExitRegion(Region region) {
                 Log.i(TAG, "I no longer see an beacon");
-                messageListAdapter.addMessage("Detected a beacon: "+region.getUniqueId());
+                broadcastIntent("Lost signal to beacon with id: " + region.getUniqueId());
             }
 
             @Override
             public void didDetermineStateForRegion(int state, Region region) {
-                Log.i(TAG, "I have just switched from seeing/not seeing beacons: "+state);
+                Log.i(TAG, "I have just switched from seeing/not seeing beacons: " + state);
+                broadcastIntent("I have just switched from seeing/not seeing beacons: " + state);
             }
         });
 
         try {
             beaconManager.startMonitoringBeaconsInRegion(new Region("myMonitoringUniqueId", null, null, null));
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
         } catch (RemoteException e) {    }
+    }
+
+    /**
+     * Send broadcast a message to messageReceiver
+     * @param message is the message that will be displayed in the activity's listview
+     */
+
+    public void broadcastIntent(String message)
+    {
+        Intent intent = new Intent();
+        intent.setAction("com.intent.MESSAGE_INTENT");
+        intent.putExtra("message", message);
+        sendBroadcast(intent);
     }
 }
