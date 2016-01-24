922c7c2ae46c377881e98bfd0b4d5a3348a1293a
Kevin Truong <truong.kevin3@gmail.com>
2016-01-23 19:18:01 -0500

    Set up the main activity to receive data from a blue tooth beacon.

--- WaterlooHacks-Android/app/src/main/java/com/kevint/waterloohacks_android/Activities/MainActivity.java
+++ WaterlooHacks-Android/app/src/main/java/com/kevint/waterloohacks_android/Activities/MainActivity.java
@@ -11,6 +11,7 @@ import info.androidhive.slidingmenu.model.NavDrawerItem;
 import info.androidhive.slidingmenu.HomeFragment;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.os.RemoteException;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -29,9 +30,19 @@ import com.kevint.waterloohacks_android.Adapters.OffersListAdapter;
 import com.kevint.waterloohacks_android.Objects.Offer;
 import com.kevint.waterloohacks_android.R;
 
-import java.util.ArrayList;
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class MainActivity extends AppCompatActivity implements BeaconConsumer {
+    protected static final String TAG = "MainActivity";
     private Context context;
 
     private ListView offersListView;
@@ -66,6 +77,17 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private BeaconManager beaconManager;
+
+    // This broadcast receiver is used to update the activity ui when the phone receives data
+    // from a bluetooth beacon.
+    BroadcastReceiver beaconReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            System.out.println(intent.getStringExtra("message"));
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -76,6 +98,7 @@ public class MainActivity extends AppCompatActivity {
         offersListAdapter = new OffersListAdapter(this, android.R.layout.simple_list_item_1, offers);
         offersListView.setAdapter(offersListAdapter);
         setUpNavMenu(savedInstanceState);
+        setUpBluetoothBeacon();
     }
 
     @Override
@@ -188,6 +211,44 @@ public class MainActivity extends AppCompatActivity {
         registerReceiver(navDrawerCloserReceiver, filter);
     }
 
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.setRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                if (beacons.size() > 0) {
+                    Log.i(TAG, "The first beacon I see is about "+beacons.iterator().next().getDistance()+" meters away.");
+                    broadcastIntent("The first beacon I see is about " + beacons.iterator().next().getDistance()+" meters away.");
+                }
+            }
+        });
+
+        beaconManager.setMonitorNotifier(new MonitorNotifier() {
+            @Override
+            public void didEnterRegion(Region region) {
+                Log.i(TAG, "I just saw an beacon for the first time!");
+                broadcastIntent("Detected a beacon with id: " + region.getUniqueId());
+            }
+
+            @Override
+            public void didExitRegion(Region region) {
+                Log.i(TAG, "I no longer see an beacon");
+                broadcastIntent("Lost signal to beacon with id: " + region.getUniqueId());
+            }
+
+            @Override
+            public void didDetermineStateForRegion(int state, Region region) {
+                Log.i(TAG, "I have just switched from seeing/not seeing beacons: " + state);
+                broadcastIntent("I have just switched from seeing/not seeing beacons: " + state);
+            }
+        });
+
+        try {
+            beaconManager.startMonitoringBeaconsInRegion(new Region("myMonitoringUniqueId", null, null, null));
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {    }
+    }
+
     /**
      * Slide menu item click listener
      * */
@@ -258,6 +319,32 @@ public class MainActivity extends AppCompatActivity {
         offers.add(offer2);
         offers.add(offer3);
     }
+
+    private void setUpBluetoothBeacon() {
+        // bluetooth beacon setup
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+        // To detect proprietary beacons, you must add a line like below corresponding to your beacon
+        // type.  Do a web search for "setBeaconLayout" to get the proper expression.
+        // beaconManager.getBeaconParsers().add(new BeaconParser().
+        // setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("m:0-3=4c000215,i:4-19,i:20-21,i:22-23,p:24-24"));
+        beaconManager.bind(this);
+
+        // register broadcast receiver
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("com.intent.BEACON_INTENT");
+        registerReceiver(beaconReceiver, filter);
+    }
+
+    public void broadcastIntent(String message)
+    {
+        Intent intent = new Intent();
+        intent.setAction("com.intent.BEACON_INTENT");
+        intent.putExtra("message", message);
+        sendBroadcast(intent);
+    }
+
     /***
      * Called when invalidateOptionsMenu() is triggered
      */
