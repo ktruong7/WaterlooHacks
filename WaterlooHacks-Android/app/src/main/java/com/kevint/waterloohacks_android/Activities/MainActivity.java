408e1e6e00d53d4fb1bebcf0636777f3c5dc6222
Kevin Truong <truong.kevin3@gmail.com>
2016-01-23 22:33:30 -0500

    Set up the main activity to load offers when it receives offer ids from a bluetooth beacon.

--- WaterlooHacks-Android/app/src/main/java/com/kevint/waterloohacks_android/Activities/MainActivity.java
+++ WaterlooHacks-Android/app/src/main/java/com/kevint/waterloohacks_android/Activities/MainActivity.java
@@ -42,6 +42,8 @@ import org.altbeacon.beacon.Region;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity implements BeaconConsumer {
     protected static final String TAG = "MainActivity";
@@ -68,6 +70,8 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
     private ArrayList<NavDrawerItem> navDrawerItems;
     private NavDrawerListAdapter adapter;
 
+    private Set<Integer> offerIds;
+
     // This broadcast receiver is used to update the activity ui when the phone receives data
     // from a bluetooth beacon.
     BroadcastReceiver navDrawerCloserReceiver = new BroadcastReceiver() {
@@ -86,6 +90,7 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
     BroadcastReceiver beaconReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
+            addOfferFromBeacon(intent.getIntExtra("offerId", 1));
         }
     };
 
@@ -94,6 +99,7 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
         super.onCreate(savedInstanceState);
         context = this;
         setContentView(R.layout.activity_main);
+        offerIds = new HashSet<>();
         offersListView = (ListView) findViewById(R.id.offers_list);
         populateOfferList();
         offersListAdapter = new OffersListAdapter(this, android.R.layout.simple_list_item_1, offers);
@@ -138,6 +144,8 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
     protected void onDestroy() {
         super.onDestroy();
         unregisterReceiver(navDrawerCloserReceiver);
+        unregisterReceiver(beaconReceiver);
+        beaconManager.unbind(this);
     }
 
     private void setUpNavMenu(Bundle savedInstanceState) {
@@ -238,6 +246,11 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
                     }
                     if(nearestBeacon != null && nearestBeacon.getId1().toHexString().equals("0xa77a1b6849a74dbf914c760d07fbb8aa")) {
                         String id3 = nearestBeacon.getId3().toHexString();
+                        int offerId = Integer.parseInt(id3.substring(2), 16);
+                        if(!offerIds.contains(offerId)) {
+                            offerIds.add(offerId);
+                            broadcastIntent(offerId);
+                        }
                     }
                     // TODO: Extract id3 from the nearest beacon and perform a look up from the offerMapper
                     // to add the new offer to the listview.
