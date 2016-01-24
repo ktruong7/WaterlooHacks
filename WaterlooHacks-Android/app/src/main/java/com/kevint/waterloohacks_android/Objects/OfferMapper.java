a9c3bf3c5d3c3591bc93b71bfb65ea60f9c6b8af
Chung Zheng <chungzheng@gmail.com>
2016-01-23 19:58:06 -0500

    added images for offers

--- WaterlooHacks-Android/app/src/main/java/com/kevint/waterloohacks_android/Objects/OfferMapper.java
+++ WaterlooHacks-Android/app/src/main/java/com/kevint/waterloohacks_android/Objects/OfferMapper.java
@@ -1,5 +1,7 @@
 package com.kevint.waterloohacks_android.Objects;
 
+import com.kevint.waterloohacks_android.R;
+
 import java.util.HashMap;
 /**
  * Created by chungzheng on 1/23/2016.
@@ -12,25 +14,25 @@ public class OfferMapper {
         offerMap = new HashMap<Integer,Offer>();
         Offer ofr;
                         // name, description, hours, Bitmap resource ID
-        ofr = new Offer("name","desc",11,123456);
+        ofr = new Offer("Auto parts Sale","desc",11, R.drawable.autoparts);
         offerMap.put(1000, ofr);
-        ofr = new Offer("name","desc",12,123456);
+        ofr = new Offer("Shoe Sale","desc",12,R.drawable.giuseppe);
         offerMap.put(1001, ofr);
-        ofr = new Offer("name","desc",13,123456);
+        ofr = new Offer("Guitar Sale","desc",13,R.drawable.bwguitar);
         offerMap.put(1002, ofr);
-        ofr = new Offer("name","desc",14,123456);
+        ofr = new Offer("Electronics Sale","desc",14,R.drawable.circuitblue);
         offerMap.put(1003, ofr);
-        ofr = new Offer("name","desc",23,123456);
+        ofr = new Offer("Battery Sale","desc",23,R.drawable.duracell);
         offerMap.put(1004, ofr);
-        ofr = new Offer("name","desc",32,123456);
+        ofr = new Offer("Produce Sale","desc",32,R.drawable.fruits_in_gardens);
         offerMap.put(1005, ofr);
-        ofr = new Offer("name","desc",48,123456);
+        ofr = new Offer("Light Bulb Sale","desc",48,R.drawable.lightbulbs);
         offerMap.put(1006, ofr);
-        ofr = new Offer("name","desc",36,123456);
+        ofr = new Offer("Graphics Card Sale","desc",36,R.drawable.nvidia);
         offerMap.put(1007, ofr);
-        ofr = new Offer("name","desc",10,123456);
+        ofr = new Offer("Chocolate Sale","desc",10,R.drawable.toblerone);
         offerMap.put(1008, ofr);
-        ofr = new Offer("name","desc",8,123456);
+        ofr = new Offer("Staionaries Sale","desc",8,R.drawable.stationary);
         offerMap.put(1009, ofr);
     }
 
